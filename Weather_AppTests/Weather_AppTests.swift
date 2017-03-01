f720b1f11a4fc72ef5c33a1929572786f502bda5
Sudhir Kumar <sudhir1.kumar@citi.com>
2017-03-01 17:29:56 -0600

    Initial Commit

---
+++ Weather_AppTests/Weather_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_AppTests.swift
+//  Weather_AppTests
+//
+//  Created by Sudhir Kumar on 3/1/17.
+//  Copyright © 2017 Macee. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_App
+
+class Weather_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
